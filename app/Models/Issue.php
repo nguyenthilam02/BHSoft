90132e7b7b2938f34d03594577cc2e562726b91c
lamnt <lamnguyenthi02@gmail.com>
2024-04-14 22:47:23 +0700

    show issue

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -22,4 +22,15 @@ class Issue extends Model
         'project_id',
         'user_id',
     ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
