f085bb0e4aac73a0b90df922081c504253a80a93
lamnt <lamnguyenthi02@gmail.com>
2024-04-14 22:47:20 +0700

    create view staff

--- resources/views/staff/create.blade.php
+++ resources/views/staff/create.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Thêm mới issue</title>
+    <title>Thêm mới nhân viên</title>
 
     <!-- Bootstrap -->
     <link href="{{asset('/vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
@@ -75,7 +75,7 @@
                     <div class="col-md-12 col-sm-12 ">
                         <div class="x_panel">
                             <div class="x_title">
-                                <h2>Thêm báo cáo</h2>
+                                <h2>Thêm nhân viên</h2>
                                 <ul class="nav navbar-right panel_toolbox">
                                     <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                     </li>
