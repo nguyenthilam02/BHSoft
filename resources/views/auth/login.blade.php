34c7ee5181073be90096b0a4bf81934e4d55af1f
lamnt <lamnguyenthi02@gmail.com>
2024-04-14 22:47:08 +0700

    change view dashboard, logout, profile

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,128 +48,79 @@
 {{--</x-guest-layout>--}}
 
 
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <!-- Required meta tags -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <title>Login Admin</title>
 
+    <!-- Bootstrap -->
+    <link href="{{asset('vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="{{asset('vendors/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="{{asset('vendors/nprogress/nprogress.css')}}" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="{{asset('vendors/animate.css/animate.min.css')}}" rel="stylesheet">
 
-    <!-- Favicon icon-->
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/favicon/favicon.ico')}}">
-
-    <!-- Libs CSS -->
-
-
-    <link href="{{asset('node_modules/bootstrap-icons/font/bootstrap-icons.css')}}" rel="stylesheet">
-    <link href="{{asset('node_modules/dropzone/dist/dropzone.css')}}" rel="stylesheet">
-    <link href="{{asset('node_modules/@mdi/font/css/materialdesignicons.min.css')}}" rel="stylesheet" />
-    <link href="{{asset('node_modules/prismjs/themes/prism-okaidia.css')}}" rel="stylesheet">
-
-    <!-- Theme CSS -->
-    <!-- build:css @@webRoot/assets/css/theme.min.css -->
-    <link rel="stylesheet" href="{{asset('assets/css/theme.css')}}">
-    <!-- endbuild -->
-    <title>Sign In | Dash Ui - Bootstrap 5 Admin Dashboard Template</title>
+    <!-- Custom Theme Style -->
+    <link href="{{asset('build/css/custom.min.css')}}" rel="stylesheet">
 </head>
 
-<body class="bg-light">
-    <div class="container d-flex flex-column">
-        <div class="row align-items-center justify-content-center g-0
-        min-vh-100">
-            <div class="col-12 col-md-8 col-lg-6 col-xxl-4 py-8 py-xl-0">
-                <!-- Card -->
-                <div class="card smooth-shadow-md">
-                    <!-- Card body -->
-                    <div class="card-body p-6">
-                        <div class="mb-4">
-                            <a href="../index.html"><img src="../assets/images/brand/logo/logo-primary.svg" class="mb-2"
-                                    alt=""></a>
-                            <p class="mb-6">Please enter your user information.</p>
-                        </div>
-                        <!-- Form -->
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-                            <!-- Username -->
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Username or email</label>
-                                <input type="email" id="email" class="form-control" name="email"
-                                    placeholder="Email address here" required>
-                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                            </div>
-                            <!-- Password -->
-                            <div class="mb-3">
-                                <label for="password" class="form-label">Password</label>
-                                <input type="password" id="password" class="form-control" name="password"
-                                    placeholder="**************" required>
-                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                            </div>
-                            <!-- Checkbox -->
-                            <div class="d-lg-flex justify-content-between align-items-center
-                  mb-4">
-                                <div class="form-check custom-checkbox">
-                                    <input type="checkbox" class="form-check-input" id="remember_me">
-                                    <label class="form-check-label" for="rememberme">Remember
-                                        me</label>
-                                </div>
+<body class="login">
+<div>
+    <a class="hiddenanchor" id="signup"></a>
+    <a class="hiddenanchor" id="signin"></a>
 
-                            </div>
-                            <div>
-                                <!-- Button -->
-                                <div class="d-grid">
-                                    <button type="submit" class="btn btn-primary">Sign
-                                        in</button>
-                                </div>
-
-                                <div class="d-md-flex justify-content-between mt-4">
-                                    <div class="mb-2 mb-md-0">
-                                        @if (Route::has('register'))
-                                            <a href="{{ route('register') }}" class="fs-5">Create An
-                                                Account </a>
-                                        @endif
-                                    </div>
-                                    <div>
-                                        <a href="{{ route('password.request') }}" class="text-inherit
-                        fs-5">Forgot your password?</a>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </form>
+    <div class="login_wrapper">
+        <div class="animate form login_form">
+            <section class="login_content">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <h1>Login Form</h1>
+                    <div>
+                        <input class="form-control" placeholder="Email" type="email" name="email" :value="old('email')"
+                               required
+                               autofocus autocomplete="username"/>
+                        <x-input-error :messages="$errors->get('email')" class="mt-2"/>
                     </div>
-                </div>
-            </div>
+                    <div>
+                        <input type="password" class="form-control" placeholder="Password" name="password" required
+                               autocomplete="current-password"/>
+                        <x-input-error :messages="$errors->get('password')" class="mt-2"/>
+                    </div>
+                    <div>
+                        <button class="btn btn-default submit" type="submit"><a>Log in</a></button>
+                        {{--                        <a class="reset_pass" href="#">Lost your password?</a>--}}
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                    <div class="separator">
+                        <p class="change_link">New to site?
+                            <a href="{{ route('register') }}" class="to_register"> Create Account </a>
+                        </p>
+
+                        <div class="clearfix"></div>
+                        <br/>
+
+{{--                        <div>--}}
+{{--                            <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>--}}
+{{--                            <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 4 template. Privacy and--}}
+{{--                                Terms</p>--}}
+{{--                        </div>--}}
+                    </div>
+                </form>
+            </section>
         </div>
+
+
     </div>
-    <!-- Scripts -->
-    <!-- Libs JS -->
-
-    <script src="{{asset('node_modules/jquery/dist/jquery.min.js')}}"></script>
-    <script src="{{asset('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('node_modules/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
-    <script src="{{asset('node_modules/feather-icons/dist/feather.min.js')}}"></script>
-    <script src="{{asset('node_modules/prismjs/prism.js')}}"></script>
-    <script src="{{asset('node_modules/apexcharts/dist/apexcharts.min.js')}}"></script>
-    <script src="{{asset('node_modules/dropzone/dist/min/dropzone.min.js')}}"></script>
-    <script src="{{asset('node_modules/prismjs/plugins/toolbar/prism-toolbar.min.js')}}"></script>
-    <script src="{{asset('node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js')}}"></script>
-
-
-
-
-    <!-- Theme JS -->
-    <!-- build:js @@webRoot/assets/js/theme.min.js -->
-    <script src="{{asset('assets/js/main.js')}}"></script>
-    <script src="{{asset('assets/js/feather.js')}}"></script>
-    <script src="{{asset('assets/js/sidebarMenu.js')}}"></script>
-
-
-
-    <!-- endbuild -->
+</div>
 </body>
-
 </html>
